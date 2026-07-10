dfc49011da064c2e82b33450da201803bfc7ec45
jagdeep jindal <jagdeepjindal007@gmail.com>
2026-07-10 13:07:48 +0530

    error

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -22,67 +22,60 @@ export const Signup = async (req, res) => {
 export const Login = async (req, res) => {
     console.log("login api called.........");
 
-    try {
+    
         const { email, password } = req.body
-
-        const user = await User.findOne({ email })
-
-        if (!user) {
-            res.status(404).json({ message: "User Not found" })
-        }
-
-        const isMatch = bcrypt.compareSync(password, user.password); // true
-
-        if (!isMatch) {
-            res.status(401).json({ message: "Invalid password" })
-        }
-
-        var token = jwt.sign({ id: user._id, name: user.name, email: user.email }, process.env.SECRET_KEY, { expiresIn: '365d' });
-
-        res.status(200).json({ message: "Login successfully", data: user, token })
-    } catch (error) {
+        
+    const authService = new AuthService()
+      const response = await authService.login({  email, password })
+if (response.status === 200) {
+        res.status(200).json({ message: "Login successfully", data: responseuser, token })
+  } else {
         console.log(error);
         res.status(500).json({ message: error.message })
-
-    }
+  }
+    
 
 }
 
 export const GetMyAccount = async (req, res) => {
+    
     const id = req.id
 
-    try {
-        const user = await User.findById(id)
-        user.password = undefined
+         
+    const authService = new AuthService()
+      const response = await authService.getMe({id})
+
+   
+     if (response.status === 200) {
         res.status(200).json({ data: user })
-    } catch (error) {
+   } else {
         console.log(error);
         res.status(500).json({ message: error.message })
-    }
+   }
 }
 
-export const Update = async (req, res) => {
-    const { id } = req.params
-    const { todo } = req.body
+// export const Update = async (req, res) => {
+//     const { id } = req.params
+//     const { todo } = req.body
 
-    try {
-        // agar 1 document nikalna hai dn se
-        const user = await User.findById(id)
+//     try {
+//         // agar 1 document nikalna hai dn se
+//         const user = await User.findById(id)
 
-        // agar sabhi document nikalna hai dn se
-        const users = await User.find({})
+//         // agar sabhi document nikalna hai dn se
+//         const users = await User.find({})
 
-        // agar 1 document ko update krna hai 
-        const user = await User.findByIdAndUpdate(id, { todo })
+//         // agar 1 document ko update krna hai 
+//         const user = await User.findByIdAndUpdate(id, { todo })
 
-        // agar mere ko 1 record delete krna hai db se
-        const user = await User.findByIdAndDelete(id)
+//         // agar mere ko 1 record delete krna hai db se
+//         const user = await User.findByIdAndDelete(id)
 
 
-        user.password = undefined
-        res.status(200).json({ data: user })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: error.message })
-    }
-}
+//         user.password = undefined
+//         res.status(200).json({ data: user })
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({ message: error.message })
+//     }
+// }
--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -1,11 +1,11 @@
-import User from "../models/user.model";
+import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken"
 //class bnai hai 
 export class AuthService {
 
     //function decleration
-    createUser(data) {
+   async createUser(data) {
         try {
 
             //password hide krn vste property 
@@ -25,11 +25,37 @@ export class AuthService {
         }
     }
 
-    login() {
+    async login() {
+        try {
+             const user = await User.findOne({ email })
+            
+                    if (!user) {
+                        res.status(404).json({ message: "User Not found" })
+                    }
+            
+                    const isMatch = bcrypt.compareSync(password, user.password); // true
+            
+                    if (!isMatch) {
+                        res.status(401).json({ message: "Invalid password" })
+                    }
+            
+                    var token = jwt.sign({ id: user._id, name: user.name, email: user.email }, process.env.SECRET_KEY, { expiresIn: '365d' });
+             return { status: 200, user, token, mesage: "Login succesfully" }
+        } catch (error) {
+            return { status: 500, mesage: error.message }
+            
+        }
 
     }
 
-    getMe() {
+   async getMe() {
+        try {
+               const user = await User.findById(id)
+                    user.password = undefined
+  return { status: 200, user }
+        } catch (error) {
+               return { status: 500, mesage: error.message }
+        }
 
     }
 
